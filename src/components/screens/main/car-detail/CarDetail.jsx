6dce195d5fcd008c26f067bce7b617b2c6c2de4b
ArtN7 <nam_artem_2004@mail.ru>
2023-11-12 20:15:49 +0300

    UPD

--- src/components/screens/main/car-detail/CarDetail.jsx
+++ src/components/screens/main/car-detail/CarDetail.jsx
@@ -1,5 +1,4 @@
 import { useParams } from "react-router-dom";
-import {carsInfo} from "../cars-info.data.js"
 import {cars as allCars} from "../cars.data.js"
 import { useState } from "react";
 import styles from "./CarDetail.module.css"
@@ -11,7 +10,7 @@ const CarDeatil = () => {
         if (Number(id) === allCars[i].id) {
             return (
                 <div className={styles.carDetail}>
-                    <div className={styles.image} style={{
+                    <div className={styles.image} alt={allCars[i].name} style={{
                         backgroundImage:`url(${allCars[i].image})`,
                         }}>
                         <a href = "../../catalogCars"><button className='btn btnBack'>Back</button></a>
@@ -20,12 +19,17 @@ const CarDeatil = () => {
                         <h2>{allCars[i].name}</h2>    
                         <p>{new Intl.NumberFormat('en-US', {
                             style: 'currency', currency: 'USD',
-                        }).format(allCars[i].price)}</p>   
+                        }).format(allCars[i].price)}</p>
                         <div>
-                            <CarDetailDescription car={carsInfo[i]}/>
+                            <CarDetailDescription car={allCars[i]}/>
                         </div>
                     </div>
                 </div>
+            )
+        }
+        else {
+            return (
+                <div className = {styles.error}>"Not found or insufficient data"</div>               
             )
         }
     }
